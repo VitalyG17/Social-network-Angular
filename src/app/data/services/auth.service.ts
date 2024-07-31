869639f52c34ae84aab38f87a17ab9f683bfa6aa
Vitaly Gulyaev <VitalyG-17@yandex.ru>
2024-07-31 16:18:40 +0300

    add: ngx-cookie-service for saving token on cookie

--- src/app/data/services/auth.service.ts
+++ src/app/data/services/auth.service.ts
@@ -3,10 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { LoginFormData } from '../interfaces/login.interface';
 import { TokenResponse } from '../interfaces/auth.interface';
 import { Observable, tap } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable()
 export class AuthService {
   private readonly http: HttpClient = inject(HttpClient);
+
+  private readonly cookieService: CookieService = inject(CookieService);
 
   private baseApiUrl: string = 'https://icherniakov.ru/yt-course/auth/';
 
@@ -26,11 +29,17 @@ export class AuthService {
         tap((value: TokenResponse) => {
           this.token = value.access_token;
           this.refreshToken = value.refresh_token;
+
+          this.cookieService.set('access_token', this.token);
+          this.cookieService.set('refresh_token', this.refreshToken);
         }),
       );
   }
 
   public isAuth(): boolean {
+    if (!this.token) {
+      this.token = this.cookieService.get('access_token');
+    }
     return Boolean(this.token);
   }
 }
